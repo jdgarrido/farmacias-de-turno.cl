692dc68ab26bcde34bb2268cf6b8f39823d3461b
jdgarrido <damian.garrido.munoz@gmail.com>
2013-04-11 13:16:46 -0300

    inicio proyecto

---
+++ 666.php
@@ -0,0 +1,109 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', TRUE);
+ini_set('display_startup_errors', TRUE);
+
+define('EOL',(PHP_SAPI == 'cli') ? PHP_EOL : '<br />');
+
+date_default_timezone_set('America/Santiago');
+
+/** Include PHPExcel_IOFactory */
+require_once 'assets/classes/PHPExcel/IOFactory.php';
+
+$objReader = PHPExcel_IOFactory::createReader("Excel2007");
+$objReader->setReadDataOnly(true);
+
+$region = (isset($_GET['r'])) ? $_GET['r'] : '';
+$n_region = '';
+
+switch ($region) {
+	case 1:
+		$file = "assets/docs/1  Región_de_Tarapaca_1º_2013 v1.xlsx";
+		$n_region = "de Tarapacá";
+		break;
+	case 2:
+		$file = "assets/docs/2 Región_de_Antofagasta_1º_2013 v1.xlsx";
+		$n_region = "de Antofagasta";
+		break;
+	case 3:
+		$file = "assets/docs/3_Region_de_Atacama_1_2013_v2_180313.xlsx";
+		$n_region = "de Atacama";
+		break;
+	case 4:
+		$file = "assets/docs/4 Región_de_Coquimbo__1º_2013 v1.xlsx";
+		$n_region = "de Coquimbo";
+		break;
+	case 5:
+		$file = "assets/docs/5 Región_de_Valparaiso_1º_2013 v4_080213.xlsx";
+		$n_region = "de Valparaíso";
+		break;
+	case 6:
+		$file = "assets/docs/6 Región_de_O'Higgins__1º_2013 v1.xlsx";
+		$n_region = "del Libertador Gral. Bernardo O’Higgins";
+		break;
+	case 7:
+		$file = "assets/docs/7 Región_del_Maule_1º_2013_v2_090113.xlsx";
+		$n_region = "del Maule";
+		break;
+	case 8:
+		$file = "assets/docs/8_Region_de_Bio_Bio_1_2013_v2_210213.xlsx";
+		$n_region = "del Biobío";
+		break;
+	case 9:
+		$file = "assets/docs/9 Región_de_Araucanía_1º_2013_v2 110113.xlsx";
+		$n_region = "de la Araucanía";
+		break;
+	case 10:
+		$file = "assets/docs/10 Región_de_Los_Lagos_1_2013_v3 060213.xlsx";
+		$n_region = "de Los Lagos";
+		break;
+	case 11:
+		$file = "assets/docs/11 Región_del_Gral_del_Campo_1°_2013 v1.xlsx";
+		$n_region = "de Aysén del Gral. Carlos Ibáñez del Campo";
+		break;
+	case 12:
+		$file = "assets/docs/12 Región_de_Magallanes_y_Antartica_Chilena_1º_2013_v1.xlsx";
+		$n_region = "de Magallanes y de la Antártica Chilena";
+		break;
+	case 14:
+		$file = "assets/docs/14 Región_de_Los_Ríos__1º_2013_v1.xlsx";
+		$n_region = "de Los Ríos";
+		break;
+	case 15:
+		$file = "assets/docs/15  Región_de_Arica_y_Parinacota_1º_2013 v1.xlsx";
+		$n_region = "Arica y Parinacota";
+		break;
+	
+	default:
+		$file = "assets/docs/13 Región_Metropolitana_1º_2013_v1.xlsx";
+		$n_region = "Metropolitana";
+		break;
+}
+//$objPHPExcel = $objReader->load("assets/docs/1  Región_de_Tarapaca_1º_2013 v1.xlsx");
+$objPHPExcel = $objReader->load($file);
+
+$aData = $objPHPExcel->getActiveSheet()->toArray(null,true,true,true);
+
+$cnt = 1;
+$hoy = $maniana = '';
+foreach($aData as $registro) {
+	
+	if($cnt>1) {
+		if( (strtotime('2013-'.$registro['F'].'-'.$registro['E'])) == strtotime( date('Y-m-d') ) ) {
+			$hoy .= '<tr>';
+			$hoy .= '<td><a href="https://maps.google.com/?q='.urlencode(strtolower($registro['D']).', '.strtolower($registro['B']).', chile').'" target="_blank">'.$registro['D'].'</a></td><td>'.$registro['B'].'</td>';
+			$hoy .= '<td>'.strftime('%a %d de %b, %Y', strtotime('2013-'.$registro['F'].'-'.$registro['E'])) .'</td>';
+			$hoy .= '<td>'.strtolower($registro['G']).'</td>';
+			$hoy .= '</tr>';
+		}
+		/*if( strtotime('2013-'.$registro['F'].'-'.$registro['E']) == strtotime( date('Y').date('m').date('d')+1 ) ) {
+			$maniana .= '<tr>';
+			$maniana .= '<td>'.$registro['D'].'</td><td>'.$registro['B'].'</td>';
+			$maniana .= '<td>'.strftime('%a %d de %b, %Y', strtotime('2013-'.$registro['F'].'-'.$registro['E'])) .'</td>';
+			$maniana .= '</tr>';
+		}*/
+		
+	}
+
+	$cnt++;
+}
--- README.md
+++ README.md
@@ -1,4 +1,29 @@
-farmacias-de-turno.cl
+Farmacias de turno
 =====================
 
-Farmacias de turno
+Este script despliega los datos diarios de las farmacias de turno por comuna entregados por el Ministerio de Salud a través Portal de Datos Públicos del Estado.
+El Min de Salud cuenta con una implementación de las farmaciás de turno, el cual lo puedes encontrar en http://www.minsal.gob.cl/portal/url/page/minsalcl/g_varios/farmacias_turno/farmacias.html
+
+
+==Cómo funciona?
+Se utiliza la clase PHPExcel para leer las hojas de cálculo que proporciona el Ministerio de Salud, finalizado el proceso, la herramienta entregará solo las filas que correspondan a la fecha en que se visualiza la información.
+
+==Cómo ejecutarlo en mi máquina local?
+*Para que esto funcione es necesario descargar PHPExcel y agregar la clase dentro de la carpeta /assets/classes/
+*Además es necesario descargar dentro de la carpeta /assets/docs/ las planillas de cálculo que se encuentran en http://datos.gob.cl/datasets/ver/1547
+*Si deseas cachear las páginas, te recomeniendo implementar un sistema de cache, como este http://www.tufuncion.com/cache-php
+*Si tu servidor usa SuPHP, te recomiendo crear un php.ini en la raiz de tu sitio, definirlo en tu .htaccess y voilá
+
+==Puedo mejorarla?
+Claro que puedes!! hazlo!!
+
+==Demo
+Lo puedes ver en acción acá http://farmacias-de-turno.josegarrido.net/
+
+==Problemas
+Al leer los archivos, nos damos cuenta de que la estandarización que se tiene es muy pobre.
+Algunos problemas
+*El campo dirección, a veces viene con la comuna incluida, otras veces viene con el nombre del local/galpón/centro comercial en el que se encuentra.
+*El valor "N°" dentro del campo Dirección no debiese existir, simplemente "nombre de la calle 123"
+*3 Hojas de datos, de los cuales 2 no tienen nformación o contienen información duplicada, con 1 basta :)
+*Los datos podrían ser entregados vía json u otro
