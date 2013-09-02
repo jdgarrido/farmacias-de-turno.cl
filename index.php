b7c14e7d50dc7469e0c55f02c752677d1edb112f
jdgarrido <damian.garrido.munoz@gmail.com>
2013-09-02 09:36:35 -0400

    fix url event tracking

--- index.php
+++ index.php
@@ -43,21 +43,21 @@
 			<!-- Collect the nav links, forms, and other content for toggling -->
 			<div class="collapse navbar-collapse navbar-ex1-collapse">
 				<ul class="nav navbar-nav">
-					<li><a href="#arica_y_parinacota" onClick="_gaq.push(['Region','click','arica_y_parinacota'])">Arica y Parinacota</a></li>
-					<li><a href="#tarapaca" onClick="_gaq.push(['Region','click','tarapaca'])">Tarapacá</a></li>
-					<li><a href="#antofagasta" onClick="_gaq.push(['Region','click','antofagasta'])">Antofagasta</a></li>
-					<li><a href="#atacama" onClick="_gaq.push(['Region','click','atacama'])">Atacama</a></li>
-					<li><a href="#coquimbo" onClick="_gaq.push(['Region','click','coquimbo'])">Coquimbo</a></li>
-					<li><a href="#valparaiso" onClick="_gaq.push(['Region','click','valparaiso'])">Valparaíso</a></li>
-					<li><a href="#metropolitana" onClick="_gaq.push(['Region','click','metropolitana'])">Metropolitana</a></li>
-					<li><a href="#ohiggins" onClick="_gaq.push(['Region','click','ohiggins'])">O'Higgins</a></li>
-					<li><a href="#maule" onClick="_gaq.push(['Region','click','maule'])">Maule</a></li>
-					<li><a href="#biobio" onClick="_gaq.push(['Region','click','biobio'])">Bio-Bio</a></li>
-					<li><a href="#araucania" onClick="_gaq.push(['Region','click','araucania'])">Araucania</a></li>
-					<li><a href="#los_rios" onClick="_gaq.push(['Region','click','los_rios'])">los Ríos</a></li>
-					<li><a href="#los_lagos" onClick="_gaq.push(['Region','click','los_lagos'])">los Lagos</a></li>
-					<li><a href="#aysen" onClick="_gaq.push(['Region','click','aysen'])">Aysen</a></li>
-					<li><a href="#magallanes" onClick="_gaq.push(['Region','click','magallanes'])">Magallanes</a></li>
+					<li><a href="#arica_y_parinacota" onClick="_gaq.push(['send', 'event', 'Region','click','arica_y_parinacota'])">Arica y Parinacota</a></li>
+					<li><a href="#tarapaca" onClick="_gaq.push(['send', 'event', 'Region','click','tarapaca'])">Tarapacá</a></li>
+					<li><a href="#antofagasta" onClick="_gaq.push(['send', 'event', 'Region','click','antofagasta'])">Antofagasta</a></li>
+					<li><a href="#atacama" onClick="_gaq.push(['send', 'event', 'Region','click','atacama'])">Atacama</a></li>
+					<li><a href="#coquimbo" onClick="_gaq.push(['send', 'event', 'Region','click','coquimbo'])">Coquimbo</a></li>
+					<li><a href="#valparaiso" onClick="_gaq.push(['send', 'event', 'Region','click','valparaiso'])">Valparaíso</a></li>
+					<li><a href="#metropolitana" onClick="_gaq.push(['send', 'event', 'Region','click','metropolitana'])">Metropolitana</a></li>
+					<li><a href="#ohiggins" onClick="_gaq.push(['send', 'event', 'Region','click','ohiggins'])">O'Higgins</a></li>
+					<li><a href="#maule" onClick="_gaq.push(['send', 'event', 'Region','click','maule'])">Maule</a></li>
+					<li><a href="#biobio" onClick="_gaq.push(['send', 'event', 'Region','click','biobio'])">Bio-Bio</a></li>
+					<li><a href="#araucania" onClick="_gaq.push(['send', 'event', 'Region','click','araucania'])">Araucania</a></li>
+					<li><a href="#los_rios" onClick="_gaq.push(['send', 'event', 'Region','click','los_rios'])">los Ríos</a></li>
+					<li><a href="#los_lagos" onClick="_gaq.push(['send', 'event', 'Region','click','los_lagos'])">los Lagos</a></li>
+					<li><a href="#aysen" onClick="_gaq.push(['send', 'event', 'Region','click','aysen'])">Aysen</a></li>
+					<li><a href="#magallanes" onClick="_gaq.push(['send', 'event', 'Region','click','magallanes'])">Magallanes</a></li>
 				</ul>
 			</div><!-- /.navbar-collapse -->
 			<?php
@@ -97,7 +97,7 @@
 						    	?>
 						    	<tr>
 						    		<td><?php echo $f->comuna ?></td>
-						    		<td><a href="http://maps.google.com?q=<?php echo urlencode($f->direccion.', '.$f->comuna) ?>" onClick="_gaq.push(['Mapa','click','<?php echo str_replace(' ', '-', $f->comuna) ?>'])" target="_blank"><?php echo $f->direccion ?></a></td>
+						    		<td><a href="http://maps.google.com?q=<?php echo urlencode($f->direccion.', '.$f->comuna) ?>" onClick="_gaq.push(['send', 'event', 'Mapa','click','<?php echo str_replace(' ', '-', $f->comuna) ?>'])" target="_blank"><?php echo $f->direccion ?></a></td>
 						    		<td><?php echo $f->farmacia ?></td>
 						    	</tr>
 						    	<?php
@@ -105,7 +105,7 @@
 						    ?>
 						    </table>
 						</div>
-						<a href="#top" onClick="_gaq.push(['Volver','click'])">volver</a>
+						<a href="#top" onClick="_gaq.push(['send', 'event', 'Volver','click'])">volver</a>
 					</div>
 					<?php
 				}
